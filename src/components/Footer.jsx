fee1af01e0908fbc6c01dc3196feb6e0c4c9f39c
MHAKing <mha.m.hassan.ali@outlook.com>
2024-08-13 22:36:17 +0500

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex justify-end px-4 mb-4 py-4 font-bold text-xl">
-            <div>&#169; 2024 software.mha.ali</div>
+            <div>&#169; 2024 software-mha</div>
         </div>
     );
 };
