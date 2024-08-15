25fc92faba57066f9b8c10e9ee85306700039f36
MHAKing <mha.m.hassan.ali@outlook.com>
2024-08-16 02:10:21 +0500

    update 3d animation

--- src/pages/three/Experience.jsx
+++ src/pages/three/Experience.jsx
@@ -1,6 +1,9 @@
 import {
+    Circle,
     Float,
+    Lathe,
     Octahedron,
+    Ring,
     RoundedBox,
     Torus,
 } from "@react-three/drei";
@@ -9,7 +12,6 @@ import { Perf } from "r3f-perf";
 import { Suspense, useRef } from "react";
 
 const Experience = () => {
-
     const groupRef = useRef(null);
 
     // const handleScroll = () => {
@@ -25,61 +27,70 @@ const Experience = () => {
     //     };
     //   }, []);
     return (
-        <div className="absolute top-0 left-0 w-full h-full pointer-events-none -z-10">
+        <div className="absolute top-0 left-0 w-full h-full pointer-events-none -z-10 hidden lg:block">
             <Canvas>
-            <color attach="background" args={["black"]} />
+                <color attach="background" args={["black"]} />
                 {/* <Perf position={'top-left'}></Perf> */}
                 <Suspense>
-                <group ref={groupRef}>
-                    <group position={[0,0,0]}>
-                    <Float
-                        speed={2}
-                        rotationIntensity={3}
-                        floatIntensity={1}
-                        floatingRange={[-0.5, 0.5]}
-                    >
-                        <RoundedBox
-                            args={[1, 1, 1]}
-                            position={[-2, -1.5, 0]}
-                            rotation={[0, Math.PI * 0.15, 0]}
-                            smoothness={1}
-                            bevelSegments={1}
-                        >
-                            <meshBasicMaterial color="#FAB972" wireframe/>
-                        </RoundedBox>
-                    </Float>
-                    <Float
-                        speed={1}
-                        rotationIntensity={5}
-                        floatIntensity={0}
-                        floatingRange={[-0.5, 0.5]}
-                    >
-                        <Octahedron args={[1, 1, 1]} position={[0, 3, 0]} scale={0.5}>
-                            <meshBasicMaterial
-                                color="#FAB972"
-                                wireframe
-                            />
-                        </Octahedron>
-                    </Float>
-                    <Float
-                        speed={1}
-                        rotationIntensity={2}
-                        floatIntensity={1}
-                        floatingRange={[-0.05, 0.5]}
-                    >
-                        <Torus
-                            args={[10, 3, 16, 30]}
-                            position={[2, -1.5, 0]}
-                            rotation={[0, -Math.PI * 0.1, 0]}
-                            scale={0.05}
-                        >
-                            <meshBasicMaterial
-                                color="#FAB972"
-                                wireframe
-                            />
-                        </Torus>
-                    </Float>
-                    </group>
+                    <group ref={groupRef}>
+                        <group position={[0, 0, 0]}>
+                            <Float
+                                speed={2}
+                                rotationIntensity={3}
+                                floatIntensity={1}
+                                floatingRange={[-0.5, 0.5]}
+                            >
+                                <RoundedBox
+                                    args={[1, 1, 1]}
+                                    position={[-2, -1.5, 0]}
+                                    rotation={[0, Math.PI * 0.15, 0]}
+                                    smoothness={1}
+                                    bevelSegments={1}
+                                    scale={0.75}
+                                >
+                                    <meshBasicMaterial
+                                        color="#FAB972"
+                                        wireframe
+                                    />
+                                </RoundedBox>
+                            </Float>
+                            <Float
+                                speed={1}
+                                rotationIntensity={5}
+                                floatIntensity={0}
+                                floatingRange={[-0.5, 0.5]}
+                            >
+                                <Lathe position={[0, 3, 0]} scale={1}>
+                                    <meshBasicMaterial
+                                        color="#FAB972"
+                                        wireframe
+                                    />
+                                </Lathe>
+                            </Float>
+                            <Float
+                                speed={2}
+                                rotationIntensity={3}
+                                floatIntensity={1}
+                                floatingRange={[-0.4, 0.4]}
+                            >
+                                <Lathe position={[2, -1, 0]} scale={0.5}>
+                                    <meshBasicMaterial
+                                        color="#FAB972"
+                                        wireframe
+                                    />
+                                </Lathe>
+                                <Octahedron
+                                    args={[1, 1, 1]}
+                                    position={[2, -1, 0]}
+                                    scale={0.5}
+                                >
+                                    <meshBasicMaterial
+                                        color="#FAB972"
+                                        wireframe
+                                    />
+                                </Octahedron>
+                            </Float>
+                        </group>
                     </group>
                 </Suspense>
             </Canvas>
