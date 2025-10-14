ef611a21d7af20be1fb7fdcb83607585a9966ddb
MHA Gaming Laptop <software.mha.ali@gmail.com>
2025-10-15 00:57:03 +0500

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+
+		paths: {
+			base: ''           // must be empty for GitHub Pages user sites
+		}
+	}
 };
 
 export default config;
